006cd32a2b492420a23cc167195489216a6e1718
MeenakshiAM <miniaalayam@gmail.com>
2026-01-04 11:05:35 +0530

    feat(user):: Backend: add buyer registration functionality

--- Backend/user-service/src/config/db.js
+++ Backend/user-service/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://unikart_admin:UniKart@123@cluster0.c7vng1l.mongodb.net/?appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("MongoDB connected (User Service)");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
